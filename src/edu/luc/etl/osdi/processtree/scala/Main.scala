fcb247ffa8923e6cd7c69d9250b243884a3eea76
Konstantin Läufer <laufer@cs.luc.edu>
2011-10-29 14:41:53 -0500

    simplified process map data structure

--- src/edu/luc/etl/osdi/processtree/scala/Main.scala
+++ src/edu/luc/etl/osdi/processtree/scala/Main.scala
@@ -10,9 +10,7 @@ object Main {
   val IO_BUF_SIZE = 8192
   val CHILD_LIST_SIZE = 16
 
-  case class Proc(pid: Int, ppid: Int, cmd: String)
-
-  def procFromLine(header: String) = {
+  def parseLine(header: String) = {
     val cols = new java.util.StringTokenizer(header).toList
     val iPid = cols indexOf "PID"
     val iPpid = cols indexOf "PPID"
@@ -23,38 +21,37 @@ object Main {
     (line: String) => {
       val sTok = new java.util.StringTokenizer(line)
       val words = (0 to max(iPid, iPpid)).map(_ => sTok.nextToken())
-      Proc(words(iPid).toInt, words(iPpid).toInt, line.substring(iCmd))
+      (words(iPid).toInt, words(iPpid).toInt, line.substring(iCmd))
     }
   }
 
   def main(args: Array[String]) = {
     val out = new BufferedWriter(new OutputStreamWriter(System.out), IO_BUF_SIZE);
     val in = new BufferedReader(new InputStreamReader(System.in), IO_BUF_SIZE)
-    val toProc = procFromLine(in.readLine())
+    val parse = parseLine(in.readLine())
 
-    val pmap = new HashMap[Int, Proc]
+    val pmap = new HashMap[Int, String]
     val tmap = new HashMap[Int, Buffer[Int]] // with MultiMap[Int, Int]
 
     val start = System.currentTimeMillis
 
     var line = in.readLine()
     while (line != null) {
-      val p = toProc(line)
-      pmap += ((p.pid, p))
+      val (pid, ppid, cmd) = parse(line)
+      pmap += ((pid, cmd))
 //      tmap.addBinding(p.ppid, p.pid)
-      if (! tmap.contains(p.ppid))
-        tmap += ((p.ppid, new ArrayBuffer[Int](CHILD_LIST_SIZE)))
-      tmap(p.ppid) += p.pid
+      if (! tmap.contains(ppid))
+        tmap += ((ppid, new ArrayBuffer[Int](CHILD_LIST_SIZE)))
+      tmap(ppid) += pid
       line = in.readLine()
      }
 
     def printTree(l: Int, i: Int) {
-      val p = pmap(i)
       for (i <- 0 until l)
     	  out.append(' ')
       out.append(i.toString)
 	  out.append(": ")
-	  out.append(p.cmd)
+	  out.append(pmap(i))
 	  out.newLine();
       if (tmap.contains(i))
         tmap(i).foreach(printTree(l + 1, _))
