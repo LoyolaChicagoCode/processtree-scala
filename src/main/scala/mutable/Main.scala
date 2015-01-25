ca5db3bd5a7720eb6ae33cd46bc381d75e2d8d32
Konstantin Läufer <laufer@cs.luc.edu>
2015-01-24 20:19:33 -0600

    cosmetic changes for consistency across versions
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- src/main/scala/mutable/Main.scala
+++ src/main/scala/mutable/Main.scala
@@ -1,7 +1,7 @@
 package edu.luc.etl.osdi.processtree.scala
 package mutable
 
-import edu.luc.etl.osdi.processtree.scala.common._
+import common._
 
 import scala.collection.mutable.{ArrayBuffer, Buffer, HashMap}
 
@@ -12,20 +12,24 @@ object Main extends App {
   
   val start = System.currentTimeMillis
   val processTree = Mutable.buildTree(processes)
-  IO.printTree(processTree)
+  val total = System.currentTimeMillis - start
 
-  println("processing time: " + System.currentTimeMillis - start  + " ms")
+  IO.printTree(processTree)
+  println("processing time: " + total + " ms")
 }
 
 object Mutable extends TreeBuilder {
+
   val CHILD_LIST_SIZE = 16
-  override def buildTree(processes: Iterator[(Int, Int, String)]): Map[Int, scala.Seq[(Int, Int, String)]] = {
-    val treeMap = new HashMap[Int, Buffer[Tuple3[Int, Int, String]]]
+
+  override def buildTree(processes: Iterator[(Int, Int, String)]): 
+    Map[Int, scala.Seq[(Int, Int, String)]] = {
+    val treeMap = new HashMap[Int, Buffer[(Int, Int, String)]]
     while (processes.hasNext) {
       val tuple = processes.next()
       val (pid, ppid, cmd) = tuple
       if (! treeMap.contains(ppid)) {
-        treeMap += ((ppid, new ArrayBuffer[Tuple3[Int, Int, String]](CHILD_LIST_SIZE)))
+        treeMap += ((ppid, new ArrayBuffer[(Int, Int, String)](CHILD_LIST_SIZE)))
       }
       treeMap(ppid) += tuple
     }
