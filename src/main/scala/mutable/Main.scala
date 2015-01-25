60077653622aca3a314ae232837b2506010ce237
Saulo Aguiar <sauloaguiar89@gmail.com>
2015-01-24 18:42:21 -0600

    Adding mutable implementation and tests

--- src/main/scala/mutable/Main.scala
+++ src/main/scala/mutable/Main.scala
@@ -1,63 +1,34 @@
 package edu.luc.etl.osdi.processtree.scala
 package mutable
 
-import java.io.{BufferedWriter, OutputStreamWriter}
+import edu.luc.etl.osdi.processtree.scala.common._
 
-import scala.collection.JavaConversions.enumerationAsScalaIterator
 import scala.collection.mutable.{ArrayBuffer, Buffer, HashMap}
-import scala.math.max
 
-object Main {
+object Main extends App {
+  val lines = scala.io.Source.stdin.getLines
+  val parse = IO.parseLine(lines.next())
+  val processes = lines map parse
+  
+  val start = System.currentTimeMillis
+  val processTree = Mutable.buildTree(processes)
+  IO.printTree(processTree)
 
-  val IO_BUF_SIZE = 8192
+  println("processing time: " + System.currentTimeMillis - start  + " ms")
+}
+
+object Mutable extends TreeBuilder {
   val CHILD_LIST_SIZE = 16
-
-  def parseLine(header: String) = {
-    val cols = new java.util.StringTokenizer(header).toList
-    val iPid = cols indexOf "PID"
-    val iPpid = cols indexOf "PPID"
-    val iCmd = max(header indexOf "CMD", header indexOf "COMMAND")
-    require(iPid >= 0, "required header field PID missing!")
-    require(iPpid >= 0, "required header field PPID missing!")
-    require(iCmd > max(iPid, iPpid), "required header field CMD or COMMAND missing or not last!")
-    (line: String) => {
-      val sTok = new java.util.StringTokenizer(line)
-      val words = (0 to max(iPid, iPpid)).map(_ => sTok.nextToken())
-      (words(iPid).toInt, words(iPpid).toInt, line.substring(iCmd))
+  override def buildTree(processes: Iterator[(Int, Int, String)]): Map[Int, scala.Seq[(Int, Int, String)]] = {
+    val treeMap = new HashMap[Int, Buffer[Tuple3[Int, Int, String]]]
+    while (processes.hasNext) {
+      val tuple = processes.next()
+      val (pid, ppid, cmd) = tuple
+      if (! treeMap.contains(ppid)) {
+        treeMap += ((ppid, new ArrayBuffer[Tuple3[Int, Int, String]](CHILD_LIST_SIZE)))
+      }
+      treeMap(ppid) += tuple
     }
-  }
-
-  def main(args: Array[String]) = {
-    val lines = scala.io.Source.stdin.getLines
-    val out = new BufferedWriter(new OutputStreamWriter(System.out), IO_BUF_SIZE);
-    val parse = parseLine(lines.next())
-
-    val pmap = new HashMap[Int, String]
-    val tmap = new HashMap[Int, Buffer[Int]]
-
-    val start = System.currentTimeMillis
-
-    for (line <- lines) {
-      val (pid, ppid, cmd) = parse(line)
-      pmap += ((pid, cmd))
-      if (! tmap.contains(ppid))
-        tmap += ((ppid, new ArrayBuffer[Int](CHILD_LIST_SIZE)))
-      tmap(ppid) += pid
-    }
-
-    println(System.currentTimeMillis - start + " ms");
-
-    def printTree(l: Int, i: Int) {
-      (1 to l) foreach { _ => out.append(' ') }
-      out.append(i.toString)
-      out.append(": ")
-      out.append(pmap(i))
-      out.newLine();
-      if (tmap.contains(i))
-        tmap(i) foreach { printTree(l + 1, _) }
-    }
-
-    tmap(0) foreach { printTree(0, _) }
-    out.flush()
+    treeMap.toMap
   }
 }
---
+++ src/test/scala/mutable/MutableSpec.scala
@@ -0,0 +1,6 @@
+package edu.luc.etl.osdi.processtree.scala
+package mutable
+
+class MutableSpec extends common.TreeBuilderSpec{
+  override val treeBuilder = Mutable
+}
