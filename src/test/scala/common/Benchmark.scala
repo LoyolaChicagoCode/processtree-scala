4dc36985fd0f7f103d16f2a855c16722d8b8eac8
Konstantin Läufer <klaeufer@gmail.com>
2016-02-29 17:36:26 -0600

    added ScalaMeter microbenchmarking
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- src/test/scala/common/Benchmark.scala
+++ src/test/scala/common/Benchmark.scala
@@ -2,10 +2,13 @@ package edu.luc.etl.osdi.processtree.scala.common
 
 import org.scalameter.api._
 
-/** Microbenchmark for `buildTree`. */
+/**
+  * Common microbenchmark superclass for `buildTree`.
+  * This cannot be a trait because it takes a constructor argument.
+  */
 abstract class Benchmark(label: String) extends Bench.LocalTime with TreeBuilder {
 
-  val sizes: Gen[Int] = Gen.exponential("processes")(100, 10000, 10)
+  val sizes: Gen[Int] = Gen.exponential("processes")(1000, 1000000, 10)
 
   val inputs: Gen[Iterator[Process]] = sizes map FakePs.fakePs
 
---
+++ src/test/scala/mutable/Benchmark.scala
@@ -0,0 +1,4 @@
+package edu.luc.etl.osdi.processtree.scala
+package mutable
+
+object Benchmark extends common.Benchmark("ProcessTree.mutable") with MutableTreeBuilder
