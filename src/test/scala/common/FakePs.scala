48981ba7beac0f7e41cd2a00c60b25ba136af209
Konstantin Läufer <klaeufer@gmail.com>
2016-02-29 17:35:24 -0600

    added fake ps generation for testing/benchmarking
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- src/test/scala/common/FakePs.scala
+++ src/test/scala/common/FakePs.scala
@@ -2,27 +2,40 @@ package edu.luc.etl.osdi.processtree.scala.common
 
 import scala.util._
 
+/**
+  * Main method to generate a fake list of processes of the specified length.
+  * Also exposes its utility methods for testing/benchmarking.
+  */
 object FakePs extends App {
-  
-  def fakePs(n: Int): Map[Int, Seq[Int]] = (2 to arg.get) .foldLeft { 
+
+  /** Generates a barebones process tree (ppid -> pid*) of size n. */
+  def fakePsMap(n: Int): Map[Int, Seq[Int]] = (2 to n).foldLeft {
     Map(0 -> Seq(1), 1 -> Seq.empty)
   } { (ps, nextPid) =>
     val randomPid = 1 + Random.nextInt(ps.keys.size - 1)
     val randomPidChildren = nextPid +: ps(randomPid)
-    val nextPidChildren = ps.getOrElse(nextPid, Seq.empty) 
+    val nextPidChildren = ps.getOrElse(nextPid, Seq.empty)
     ps + (nextPid -> nextPidChildren, randomPid -> randomPidChildren)
   }
-  
+
+  /** Returns an iterator over the inverse edges of a tree represented as a multimap. */
+  def inverseEdges(m: Map[Int, Seq[Int]]): Iterator[(Int, Int)] =
+    for (ppid <- m.keys.iterator; pid <- m(ppid).iterator) yield (pid, ppid)
+
+  def fakePs(n: Int, s: String): Iterator[Process] =
+    inverseEdges(fakePsMap(n)).map { case (pid, ppid) => (pid, ppid, s + " " + pid)}
+
+  def fakePs(n: Int): Iterator[Process] = fakePs(n, "Fake Process")
+
   val arg = Try { args(0).toInt }
   if (arg.isFailure) {
     Console.err.println("usage: fakeps n (where n = number of process table entries)")
     System.exit(1)
   }
 
+  val n = arg.get
   val ps = fakePs(arg.get)
-  
-  println("%-10s %-10s %-10s".format("PID","PPID","CMD"))
-  val pids = for (pid <- ps.keys.toSeq ; cpid <- ps(pid)) yield (cpid, pid)
-  for ((cpid, pid) <- pids.sorted)
-    println("%-10s %-10s Fake Process %s".format(cpid, pid, cpid))
+  println("%-10s %-10s %-10s".format("PID", "PPID", "CMD"))
+  for ((pid, ppid, cmd) <- ps)
+    println("%-10s %-10s %s".format(pid, ppid, cmd))
 }
